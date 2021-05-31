c8e45ff5864beb82346016bac1924483131559c5
pyrazine <ferdydendy22@gmail.com>
2021-05-31 10:22:46 +0000

    tissot: ArrowOS

--- arrow_tissot.mk
+++ arrow_tissot.mk
@@ -18,15 +18,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from mido device
+# Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions
+TARGET_BOOT_ANIMATION_RES := 1080
+DEVICE_MAINTAINER := pyrazine
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := aosp_tissot
+PRODUCT_NAME := arrow_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -40,17 +42,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
-
-# Pixel Stuffs
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-
-# PixelPlusUI
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.ppui.device_name=Mi A1 \
-ro.ppui.version=3.4 \
-ro.ppui.version_code=Onyx \
-ro.ppui.maintainer_name=pyrazine
